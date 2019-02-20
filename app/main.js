dc95a73e90c1b22f72b89c5f8718bd8f11dcb44f
Martin Leopold <m@martinleopold.com>
2019-02-20 17:16:32 +0100

    Added instanced color and size

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ import * as util from './util.js';
 const W = 1280;
 const H = 800;
 
-let renderer, scene, camera;
+let renderer, scene, camera, ocamera;
 let controls; // eslint-disable-line no-unused-vars
 
 const shader = {
@@ -12,31 +12,26 @@ const shader = {
     uniform mat4 modelViewMatrix;
     uniform mat4 projectionMatrix;
     // uniform float time;
-    uniform float scale;
     
     attribute vec3 position;
     attribute vec3 offset;
-    // attribute vec4 color;
+    attribute vec4 color;
+    attribute float size;
     
-    // varying vec3 vPosition;
-    // varying vec4 vColor;
+    varying vec4 vColor;
     
     void main() {
-      vec3 vPosition = position * scale + offset;
-      // vColor = color;
+      vColor = color;
+      vec3 vPosition = position * size + offset;
       gl_Position = projectionMatrix * modelViewMatrix * vec4( vPosition, 1.0 );
     }`,
   fs: `
     precision highp float;
     // uniform float time;
-    uniform vec3 color;
-    uniform float opacity;
-    
-    // varying vec3 vPosition;
-    // varying vec4 vColor;
+    varying vec4 vColor;
     
     void main() {
-      gl_FragColor = vec4(color, opacity);
+      gl_FragColor = vColor;
     }`
 };
 
@@ -62,34 +57,33 @@ function setup() {
   
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 75, W / H, 0.01, 1000 );
+  camera.position.z = 1;
+  // ocamera = new THREE.OrthographicCamera( W/-2, W/2, H/2, H/-2, 0, 100 );
   controls = new THREE.OrbitControls( camera, renderer.domElement );
-  camera.position.z = 2;
   
-  
-  const maxParticles = 100;
-  const particleScale = 10;
+  const maxParticles = 10000;
   const particleDetail = 8;
   
   let offsets = [];
+  let colors = [];
+  let sizes = [];
   for (let i=0; i<maxParticles; i++) {
-    offsets.push( Math.random()*2-1, Math.random()*2-1, Math.random()*2-1 );
+    offsets.push( (Math.random()*2-1)*1, (Math.random()*2-1)*1, 0 );
+    colors.push( 0, 0, Math.random()+0.3, 0.3 );
+    sizes.push( Math.random()*0.01 );
   }
   
-  let circle = new THREE.CircleGeometry( 0.01, particleDetail );
-  let geo = new THREE.BufferGeometry().fromGeometry(circle);
-  let mat = new THREE.MeshBasicMaterial({ color: 0x1e90ff, wireframe: false });
-  
+  let circle = new THREE.CircleGeometry( 0.5, particleDetail );
   let igeo = new THREE.InstancedBufferGeometry().fromGeometry(circle);
   igeo.addAttribute( 'offset', new THREE.InstancedBufferAttribute(new Float32Array(offsets), 3) );
+  igeo.addAttribute( 'color', new THREE.InstancedBufferAttribute(new Float32Array(colors), 4) );
+  igeo.addAttribute( 'size', new THREE.InstancedBufferAttribute(new Float32Array(sizes), 1) );
   // igeo.maxInstancedCount = 3;
   
   
   let imat = new THREE.RawShaderMaterial( {
     uniforms: {
-      "time":    { value: 1.0 },
-      "color":   { value: new THREE.Color(0x1e90ff) },
-      "opacity": { value: 0.3 },
-      "scale":   { value: particleScale }
+      "time": { value: 1.0 },
     },
     vertexShader: shader.vs,
     fragmentShader: shader.fs,
@@ -114,13 +108,13 @@ function loop(time) { // eslint-disable-line no-unused-vars
 
 document.addEventListener('keydown', e => {
   // console.log(e.key, e.keyCode, e);
-  
+
   if (e.key == 'f') { // f .. fullscreen
     util.toggleFullscreen();
   }
-  
+
   else if (e.key == 's') { // s .. save frame
     util.saveCanvas();
   }
-  
+
 });
